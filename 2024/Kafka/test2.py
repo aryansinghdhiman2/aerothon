1a306b453a4fc5c0aaf67b7b1564b34791100ff8
capboyaryan <96912152+capboyaryan@users.noreply.github.com>
2024-10-23 02:30:55 +0530

    stuff

--- 2024/Kafka/test2.py
+++ 2024/Kafka/test2.py
@@ -1,4 +1,20 @@
-import dronekit_sitl
+from drone_helper import connect_to_drone,moveToAlt,GUIDED,LocationGlobal
+import time
+from dronekit import LocationGlobalRelative
 
-sitl = dronekit_sitl.start_default()
-print(sitl.connection_string())
+# vehicle = connect_to_drone("tcp:localhost:5762")
+vehicle = connect_to_drone("udpout:10.42.0.1:10000")
+
+# vehicle.message_factory.system_time_send(time.time(),time.time())
+
+
+print("Going")
+vehicle.mode = GUIDED
+
+# l = LocationGlobalRelative(-30.7479417,76.7565145,20)
+
+moveToAlt(vehicle,30.7477394,76.7567633,15)
+# vehicle.simple_goto(l)
+# print(vehicle.location.global_frame)
+# print(vehicle.location.local_frame)
+# print(vehicle.location.global_relative_frame)
