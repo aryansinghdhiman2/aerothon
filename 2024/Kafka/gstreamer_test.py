71da16714b4a0ee8bcf644391ef92b34dbf8a0cd
capboyaryan <96912152+capboyaryan@users.noreply.github.com>
2024-10-25 14:41:15 +0530

    added gstreamer test file

---
+++ 2024/Kafka/gstreamer_test.py
@@ -0,0 +1,27 @@
+import cv2
+
+# Open the default camera
+cam = cv2.VideoCapture("rtspsrc location=rtsp://10.42.0.1:8554/cam latency=0 protocols=tcp ! decodebin ! videoconvert ! appsink",cv2.CAP_GSTREAMER)
+
+# print(cam.getBackendName())
+
+# Get the default frame width and height
+frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+
+while True:
+    ret, frame = cam.read()
+
+
+
+    # Display the captured frame
+    cv2.imshow('Camera', frame)
+
+    # Press 'q' to exit the loop
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# Release the capture and writer objects
+cam.release()
+cv2.destroyAllWindows()
