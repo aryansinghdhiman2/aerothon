047f18fa1c4574c17a09f72132eb54f2d7d75618
capboyaryan <96912152+capboyaryan@users.noreply.github.com>
2024-10-28 02:37:25 +0530

    stuff

--- 2024/Kafka/temp.py
+++ 2024/Kafka/temp.py
@@ -9,9 +9,8 @@ from drone_helper import connect_to_drone, goto_center, AUTO, GUIDED, Vehicle, a
 from classificationEnum import TARGET
 from classificationEnum import HOTSPOT, TARGET, DET_OBJ
 from drone_helper import connect_to_drone, getCurrentLocation
-import time
 from socketio.exceptions import TimeoutError
-
+import torch
 
 alignment_flag = [True, False, False, False]
 alignment_state = 0
@@ -41,8 +40,8 @@ async def emit_alignment(alignment_state, location, center):
     print("EMIT ENDED")
 
 
-vehicle: Vehicle = connect_to_drone("tcp:localhost:5762")
-# vehicle: Vehicle = connect_to_drone("udpout:10.42.0.1:10000")
+# vehicle: Vehicle = connect_to_drone("tcp:172.24.240.1:5762")
+vehicle: Vehicle = connect_to_drone("udpout:10.42.0.1:10000")
 
 
 MODEL_NAMES = ["./OptimizedWeights/best.pt"]
@@ -55,137 +54,149 @@ async def run_tracker_in_thread(model_name, filename):
     global alignment_state
     global alignment_flag
     frame_cnt = 0
-    model = YOLO(model_name)
-    results = model.track(filename, save=True,
-                          stream=True, conf=0.70, iou=0.65)
+    # cam = cv2.VideoCapture("rtspsrc location=rtsp://10.42.0.1:8554/cam latency=0 protocols=tcp ! decodebin ! videoconvert ! appsink drop=1 max-buffers=5 max-bytes=1843488", cv2.CAP_GSTREAMER)
+    cam = cv2.VideoCapture("rtspsrc location=rtsp://172.24.240.1:8554/cam latency=0 protocols=tcp ! decodebin ! videoconvert ! appsink drop=1 max-buffers=5 max-bytes=1843488", cv2.CAP_GSTREAMER)
+    # cam = cv2.VideoCapture("filesrc location=/mnt/c/Users/91798/Desktop/test_yolo/aerothon_repo/aerothon/2024/Kafka/output16.avi ! decodebin ! videoconvert ! appsink", cv2.CAP_GSTREAMER)
+    # time.sleep(3)
+    model = YOLO(model_name) 
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
+    model = model.to(device)
+    if not cam.isOpened():
+        print("Error: Cannot open RTSP stream.")
+        exit()
 
-    for r in results:
-        await asyncio.sleep(0)
-        locationObj = getCurrentLocation(vehicle)
-        location = [locationObj.lat, locationObj.lon,
-                    locationObj.alt, vehicle.heading]
-        print(f"Frame count - {frame_cnt}")
-        print("Location is - :::: ", location)
-        frame_cnt += 1
-        obj = (json.loads(r.to_json()))
-        im_array = r.plot()
-        conv_obj = {}
-        if len(obj) > 0:
-            conv_obj = {
-                "boxes": [obj[0]['box']],
-                "labels": [obj[0]['class']],
-                "scores": [obj[0]['confidence']]
-            }
-        else:
-            conv_obj = {
-                "boxes": [],
-                "labels": "none",
-                "scores": []
-            }
-        draw_box = (0, 0)
-        if len(conv_obj["scores"]) > 0:
-            for box in conv_obj["boxes"]:
-                print(box)
-                x1 = box["x1"]
-                x2 = box["x2"]
-                y1 = box["y1"]
-                y2 = box["y2"]
-                draw_box = (((x1+x2)/2), ((y1+y2)/2))
-        cv2.namedWindow("Yolo detection", cv2.WINDOW_NORMAL)
-        cv2.circle(im_array, (int(draw_box[0]), int(
-            draw_box[1])), 2, thickness=1, color=(0, 0, 255))
-        cv2.putText(im_array, f"({draw_box[0]},{draw_box[1]})", (int(draw_box[0]), int(
-            draw_box[1])), fontScale=1, fontFace=cv2.FONT_HERSHEY_SIMPLEX, lineType=cv2.LINE_AA, color=(0, 255, 0))
-        cv2.putText(
-            im_array, f"lat and long({location[0]},{location[1]})", (640, 25), fontScale=1, fontFace=cv2.FONT_HERSHEY_SIMPLEX, lineType=cv2.LINE_AA, color=(0, 255, 0))
-        cv2.imshow("Yolo detection", im_array)
+    frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+    while True:
+        ret, frame = cam.read()
+        if not ret:
+            print("Failed to grab frame.")
             break
 
-        with open("./json_output.jsonl", "a") as outfile:
-            res = conv_obj
-            if len(res["scores"]) == 0:
-                json.dump({
-                    "box": "None",
-                    "label": "None",
-                    "predicted_confidence_score": "None",
-                    "location": []
-                }, outfile)
-                outfile.write('\n')
+        results = model(frame,conf=0.75, iou=0.70)  
+        annotated_frame = results[0].plot()  
+        for r in results:
+            await asyncio.sleep(0)
+            locationObj = getCurrentLocation(vehicle)
+            location = [locationObj.lat, locationObj.lon,
+                        locationObj.alt, vehicle.heading]
+            print(f"Frame count - {frame_cnt}")
+            print("Location is - :::: ", location)
+            frame_cnt += 1
+            obj = (json.loads(r.to_json()))
+            im_array = r.plot()
+            conv_obj = {}
+            if len(obj) > 0:
+                conv_obj = {
+                    "boxes": [obj[0]['box']],
+                    "labels": [obj[0]['class']],
+                    "scores": [obj[0]['confidence']]
+                }
+            else:
+                conv_obj = {
+                    "boxes": [],
+                    "labels": "none",
+                    "scores": []
+                }
+            draw_box = (0, 0)
+            if len(conv_obj["scores"]) > 0:
+                for box in conv_obj["boxes"]:
+                    print(box)
+                    x1 = box["x1"]
+                    x2 = box["x2"]
+                    y1 = box["y1"]
+                    y2 = box["y2"]
+                    draw_box = (((x1+x2)/2), ((y1+y2)/2))
+            cv2.namedWindow("Yolo detection", cv2.WINDOW_NORMAL)
+            cv2.circle(annotated_frame, (int(draw_box[0]), int(
+                draw_box[1])), 2, thickness=1, color=(0, 0, 255))
+            cv2.putText(annotated_frame, f"({draw_box[0]},{draw_box[1]})", (int(draw_box[0]), int(
+                draw_box[1])), fontScale=1, fontFace=cv2.FONT_HERSHEY_SIMPLEX, lineType=cv2.LINE_AA, color=(0, 255, 0))
+            cv2.putText(
+                annotated_frame, f"lat and long({location[0]},{location[1]})", (640, 25), fontScale=1, fontFace=cv2.FONT_HERSHEY_SIMPLEX, lineType=cv2.LINE_AA, color=(0, 255, 0))
+            cv2.imshow("Yolo detection", annotated_frame)
 
-            for score, label, box in zip(res["scores"], res["labels"], res["boxes"]):
-                box = (box['x1'], box['y1'],
-                       box['x2'], box['y2'])
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-                x_min, y_min, x_max, y_max = box
-                center_x = (x_min + x_max) / 2
-                center_y = (y_min + y_max) / 2
+            # print("printing")
+            with open("./json_output.jsonl", "a") as outfile:
+                res = conv_obj
+                if len(res["scores"]) == 0:
+                    json.dump({
+                        "box": "None",
+                        "label": "None",
+                        "predicted_confidence_score": "None",
+                        "location": []
+                    }, outfile)
+                    outfile.write('\n')
 
-                center = (center_x, center_y)
-                predicted_label = label
-                predicted_confidence_score = score
-                json.dump({
-                    "box": box,
-                    "label": predicted_label,
-                    "predicted_confidence_score": predicted_confidence_score,
-                    "center": center
-                }, outfile)
-                outfile.write('\n')
+                for score, label, box in zip(res["scores"], res["labels"], res["boxes"]):
+                    box = (box['x1'], box['y1'],
+                        box['x2'], box['y2'])
 
-                if label == 1:
-                    print('Target found')
-                    # adjusting according to input resolution
-                    adjusted_center = [center_x, (480 - center_y)]
-                    print(
-                        f"Center {center}, adjusted_center {adjusted_center} location {location}")
+                    x_min, y_min, x_max, y_max = box
+                    center_x = (x_min + x_max) / 2
+                    center_y = (y_min + y_max) / 2
 
-                    # check alignment request state
-                    if (alignment_state <= 2):
-                        if (vehicle.mode == AUTO or vehicle.mode == GUIDED):
-                            print("VEHICLE MODE", vehicle.mode)
-                            if (alignment_flag[0]):
-                                print("sent 0")
-                                await emit_alignment(alignment_state,
-                                                     location, adjusted_center)
-                                alignment_flag[0] = False
-                            elif (alignment_flag[1]):
-                                print("sent 1")
-                                adjusted_center = [center_x-320, 240-center_y]
-                                await emit_alignment(alignment_state,
-                                                     location, adjusted_center)
+                    center = (center_x, center_y)
+                    predicted_label = label
+                    predicted_confidence_score = score
+                    json.dump({
+                        "box": box,
+                        "label": predicted_label,
+                        "predicted_confidence_score": predicted_confidence_score,
+                        "center": center
+                    }, outfile)
+                    outfile.write('\n')
 
-                            elif (alignment_flag[2]):
-                                alignment_flag[1] = False
-                                print("sent 2")
-                                # await emit_alignment(alignment_state,
-                                #                      location, adjusted_center)
+                    if label == 1:
+                        print('Target found')
+                        # adjusting according to input resolution
+                        adjusted_center = [center_x, (480 - center_y)]
+                        print(
+                            f"Center {center}, adjusted_center {adjusted_center} location {location}")
 
-                                alignment_flag[2] = False
+                        # check alignment request state
+                        if (alignment_state <= 2):
+                            if (vehicle.mode == AUTO or vehicle.mode == GUIDED):
+                                print("VEHICLE MODE", vehicle.mode)
+                                if (alignment_flag[0]):
+                                    print("sent 0")
+                                    await emit_alignment(alignment_state,
+                                                        location, adjusted_center)
+                                    alignment_flag[0] = False
+                                elif (alignment_flag[1]):
+                                    print("sent 1")
+                                    adjusted_center = [center_x-320, 240-center_y]
+                                    await emit_alignment(alignment_state,
+                                                        location, adjusted_center)
 
-                elif label == 0:
-                    json_obj = {
-                        "center": center,
-                        "location": location,
-                        "type": HOTSPOT
-                    }
-                    print("Hotspot found")
+                                elif (alignment_flag[2]):
+                                    alignment_flag[1] = False
+                                    print("sent 2")
 
-        print(type(results), results)
-        pass
+                                    alignment_flag[2] = False
+
+                    elif label == 0:
+                        json_obj = {
+                            "center": center,
+                            "location": location,
+                            "type": HOTSPOT
+                        }
+                        print("Hotspot found")
+
+            # print(type(results), results)
+            pass
+
+
 
 
 async def main():
-    # tracker_threads = []
     for video_file, model_name in zip(SOURCES, MODEL_NAMES):
         await run_tracker_in_thread(model_name, video_file)
-        # thread = threading.Thread(target=run_tracker_in_thread, args=(
-        #     model_name, video_file), daemon=True)
-        # tracker_threads.append(thread)
-        # thread.start()
-
-    # for thread in tracker_threads:
-    #     thread.join()
 
     cv2.destroyAllWindows()
 
