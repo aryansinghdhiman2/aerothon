dd2ba141d0de984440954dbc8aa5403bbe07f6d7
capboyaryan <96912152+capboyaryan@users.noreply.github.com>
2024-10-29 13:00:32 +0530

    stuff2

--- 2024/Kafka/testdrone.py
+++ 2024/Kafka/testdrone.py
@@ -1,6 +1,6 @@
 from drone_helper import *
 from math import degrees
-from pymavlink.dialects.v10.common import MAV_FRAME_BODY_FRD,MAV_CMD_DO_REPOSITION,MAV_FRAME_LOCAL_NED,MAV_CMD_NAV_WAYPOINT,MAV_FRAME_GLOBAL_RELATIVE_ALT,MAV_FRAME_LOCAL_FRD,MAV_FRAME_BODY_NED
+from pymavlink.dialects.v10.common import MAV_FRAME_BODY_FRD,MAV_CMD_DO_REPOSITION,MAV_FRAME_LOCAL_NED,MAV_CMD_NAV_WAYPOINT,MAV_FRAME_GLOBAL_RELATIVE_ALT,MAV_FRAME_LOCAL_FRD,MAV_FRAME_BODY_NED,MAV_FRAME_BODY_OFFSET_NED
 from coordinate_conversion import calculate_body_ned_coordinates
 
 
@@ -24,21 +24,38 @@ print(" Mode: %s" % vehicle.mode.name)    # settable
 # print(vehicle.channels.overrides)
 # vehicle.channels.overrides['15'] = 1100
 
-print("sending")
+# print("sending")
 # # vehicle.message_factory.command_int_send(0,0,MAV_FRAME_LOCAL_NED,MAV_CMD_DO_REPOSITION,0,0,4,0,0,0,10,5,-20)
 # vehicle.message_factory.command_int_send(1,1,MAV_FRAME_GLOBAL,MAV_CMD_DO_REPOSITION,0,0,10,0,0,0,int(30.7481128 * 10**7), int(76.7565859 * 10**7),15)
-x,y = calculate_body_ned_coordinates(vehicle.location.global_relative_frame.alt,vehicle.heading,0,-480)
-print(x,y)
-msg = vehicle.message_factory.set_position_target_local_ned_encode(
-0,
-0, 0,
-MAV_FRAME_BODY_NED,  #relative to drone heading pos relative to EKF origin
-0b0000000000000000,
-x, y, 0,
-0, 0,0,
-0, 0, 0,
-0, 0)
-vehicle.send_mavlink(msg)
+# x,y = calculate_body_ned_coordinates(vehicle.location.global_relative_frame.alt,vehicle.heading,0,-480)
+# print(x,y)
+# msg = vehicle.message_factory.set_position_target_local_ned_encode(
+# 0,
+# 0, 0,
+# MAV_FRAME_BODY_NED,  #relative to drone heading pos relative to EKF origin
+# 0b0000000000000000,
+# x, y, 0,
+# 0, 0,0,
+# 0, 0, 0,
+# 0, 0)
+# vehicle.send_mavlink(msg)
+
+while(True):
+
+    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+    0,
+    0, 0,
+    MAV_FRAME_BODY_OFFSET_NED,  #relative to drone heading pos relative to EKF origin
+    0b0000011111000111,
+    0, 0, 0,
+    0, 10,0,
+    0, 0, 0,
+    0, 0)
+    vehicle.send_mavlink(msg)
+    # goto_center_body_ned(vehicle,15,)
+    print("sending")
+    break
+
 
 # print(vehicle.heading)
 # print(vehicle._yaw)
